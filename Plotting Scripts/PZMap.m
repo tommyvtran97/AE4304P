0842c1022db575c7e00f0844ea6222f60186cfac
tommyvtran97 <tommyvtran97@gmail.com>
2020-02-17 12:58:49 +0100

    Implemented Variance Calculations V1

--- Plotting Scripts/PZMap.m
+++ Plotting Scripts/PZMap.m
@@ -1,15 +1,22 @@
 figure(3)
-pzmap(aircraft)
+pzmap(aircraft, 'r')
 grid on
 set(gcf, 'Position',  [0, 0, 1100, 600])
+hm = findobj(gca, 'Type', 'Line');          % Handle To 'Line' Objects
+hm(2).MarkerSize = 20;                      % Zero Marker
+hm(3).MarkerSize = 20;                      % Pole Marker
 if save == 'y'
 saveas(gcf,[pwd,'\Plots\Pz_aircraft'],'epsc');
 end
 
 figure(4)
-pzmap(aircraft_pd)
+pzmap(aircraft_pd, 'g')
 grid on
 set(gcf, 'Position',  [0, 0, 1100, 600])
+hm = findobj(gca, 'Type', 'Line');          % Handle To 'Line' Objects
+hm(2).MarkerSize = 20;                      % Zero Marker
+hm(3).MarkerSize = 20;                      % Pole Marker
 if save == 'y'
 saveas(gcf,[pwd,'\Plots\Pz_pd_aircraft'],'epsc');
 end 
+
